76922f7056c02e89f541592052854891ea504432
micalexander <micalexander@hotmail.com>
2016-10-26 20:51:18 -0700

    formatted project command

--- lib/base/format.rb
+++ lib/base/format.rb
@@ -3,54 +3,102 @@ module SwiftlyPivotal
 
     include Helpers
 
+    #
+    # Filter project before sending it to be rendered
+    # @param project [hash] Hash of project
+    #
+    # @return [void]
     def self.project project
 
+      # Check to see if a project was passed in
+      if project
+
+        # If so, send project to get rendered to the screen
+        self.render_hash({
+          '#'         => 1,
+          'ID'        => project['id'],
+          'Name'      => project['name'],
+          'Points'    => project['point_scale'],
+          'Velocity'  => project['velocity_averaged_over'],
+          'Created'   => Time.iso8601(project['created_at']).strftime('%B %e,%l:%M %p'),
+          'Updated'   => Time.iso8601(project['updated_at']).strftime('%B %e,%l:%M %p'),
+        })
+      end
     end
 
-    def self.stories stories
+    #
+    # Filter stories before sending them to be rendered
+    # @param stories [hash] Hash of stories
+    # @param number = 0 [int] Number of the story as a counter
+    #
+    # @return [void]
+    def self.stories stories, number = 0
 
-      thor   = Thor.new
-      number = 0
-
+      # Check to see if any stories were passed in
       if stories
-
 
         stories.each do |story|
 
-          numbered = false
-          message  = {
-            '#'         => thor.set_color( number = number+1, :blue, :bold ),
+          # If so, send story to get rendered to the screen
+          self.render_hash({
+            '#'         => number = number+1,
             'ID'        => story['id'],
             'Name'      => story['name'],
             'Estimated' => story['estimate'],
             'Currently' => story['current_state'].capitalize,
             'Updated'   => Time.iso8601(story['updated_at']).strftime('%B %e,%l:%M %p'),
             'URL'       => story['url']
-          }
-
-          thor.say
-
-          message.each do |k, v|
-
-            if !numbered
-
-              underline = '--'
-              thor.say thor.set_color("\t#{k.rjust(11)}#{v}", :blue, :bold )
-              thor.say thor.set_color("\t#{underline.rjust(12)}", :blue, :bold )
-
-            else
-              thor.say "\t#{k.rjust(10)}: #{v}"
-
-            end
-
-
-            numbered = true
-          end
-
-          thor.say
-
+          })
         end
       end
+    end
+
+
+    #
+    # Render a has to the screen formatted nicely
+    # @param message [hash] A hash of fields to output
+    # @param number = 0 [int] [description]
+    #
+    # @return [echo] Renders to the screen
+    def self.render_hash message
+
+      # Instantiate Thor
+      thor     = Thor.new
+
+      # Set number to false indicating that
+      # it has not been outputted yet
+      numbered = false
+
+      # Output a spacer
+      thor.say
+
+      # Loop though set of key, values of the hash
+      message.each do |k, v|
+
+        # Check to see if the number has been displayed
+        if !numbered
+
+          # If not, set first line to blue and output it
+          thor.say thor.set_color("\t#{k.rjust(11)}#{v}", :blue, :bold )
+
+          # Add an underline under the first line
+          underline = '--'
+
+          # Set underline to blue and output it
+          thor.say thor.set_color("\t#{underline.rjust(12)}", :blue, :bold )
+        else
+
+          # If so then just output it normally
+          thor.say "\t#{k.rjust(10)}: #{v}"
+        end
+
+        # Set number to true to indicate that
+        # it has already been outputted
+        numbered = true
+      end
+
+      # Output a spacer
+      thor.say
     end
   end
 end
--- lib/base/track.rb
+++ lib/base/track.rb
@@ -13,7 +13,11 @@ module SwiftlyPivotal
 
     def project( project_name )
 
+      # Go out and get the project by project name
       project = SwiftlyPivotal::PivotalTracker.project
+
+      # Format the project for display
+      SwiftlyPivotal::Format.project project
 
     end
 
@@ -26,10 +30,50 @@ module SwiftlyPivotal
     # @return [void]
     def stories( project_name )
 
+      # Go out and get 5 stories
       stories = SwiftlyPivotal::PivotalTracker.stories
 
+      # Format the retrieved stories
       SwiftlyPivotal::Format.stories stories
 
+      # Start the loaded at zero
+      loaded = 0
+
+      # Set the initial allowed answers
+      allowed = ['y', '1', '2', '3', '4', '5']
+
+      # Ask a question and as long as the answers to the question are allowed
+      while answer = ask(set_color('Load 5 more or enter number to expand', :yellow, :bold ),  :limited_to => allowed)
+
+        # Increment the loaded by 5
+        loaded = loaded + 5
+
+        # Go out and get 5 more stories
+        stories = SwiftlyPivotal::PivotalTracker.stories loaded
+
+        # Loop throut the loaded numbers
+        loaded.times do |number|
+
+          # Add them to the allowed array
+          allowed << stories.length + (number+1)
+        end
+
+        # Format the retrieved stories
+        SwiftlyPivotal::Format.stories stories, loaded
+
+        # Jump out of this loop if there are no more stories
+        break unless stories.length > 4
+
+        # Check to see if a number was passed
+        if answer.to_i  > 0
+
+          # If so, do something with it and break out of this loop
+          break
+
+        end
+      end
+
+      say 'No more stories'
     end
   end
 end
